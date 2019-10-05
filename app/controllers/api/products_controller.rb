fa737599f622e964e7af9843b2b7a15cb636e391
Chelsie Conrad <chelsiechey@gmail.com>
2019-10-05 10:30:54 -0600

    controllers

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,38 @@
 class Api::ProductsController < ApplicationController
+  before_action :set_department
+  def index
+    render json: Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product
+    else
+      render json: { errors: @product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: { errors: @product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    render json: { message: 'Product deleted'}
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :stock)
+    end
+
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
 end
